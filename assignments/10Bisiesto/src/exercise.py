414be7019e9b8626dc07e8280b210890a605faca
LuisFernandoGonzalezCortes <88851274+LuisFernandoGonzalezCortes@users.noreply.github.com>
2021-08-29 23:38:47 -0500

    Update exercise.py

--- assignments/10Bisiesto/src/exercise.py
+++ assignments/10Bisiesto/src/exercise.py
@@ -1,7 +1,18 @@
-
+def es_bisiesto(año):
+    if año % 4 == 0:
+        return True
+    elif año % 100 == 0:
+        return False
+    elif año % 400 == 0:
+        return True
+    else:
+        return False
+    
 def main():
-    #escribe tu código abajo de esta línea
-    pass
 
-if __name__=='__main__':
+    año=int(input("Dame un año: "))
+
+    print("El año es bisiesto? ",es_bisiesto(año))
+    
+if __name__ == '__main__':
     main()
